a712ddf5cc41d799eccae2c18195382d99de8985
meenahoda <meena.hoda@wmfs.net>
2022-02-25 15:32:43 +0000

    feat: Support string-paths inside arrays

--- test/resolver-tests.js
+++ test/resolver-tests.js
@@ -10,6 +10,70 @@ const resolver = require('./../lib')
 const tests = [
   {
     input: {
+      selected: [
+        { uprn: 1 },
+        { uprn: 2 },
+        { uprn: 3 }
+      ]
+    },
+    template: {
+      uprn: '$.uprn',
+      numSelected: '$.selected.length',
+      'selected.$q': '$.selected[*].uprn'
+    },
+    expected: {
+      uprn: undefined,
+      numSelected: 3,
+      selected: [1, 2, 3]
+    }
+  },
+  {
+    input: {
+      selected: [
+        { uprn: 1 },
+        {},
+        { uprn: 3 }
+      ]
+    },
+    template: {
+      numSelected: '$.selected.length',
+      'selected.$q': '$.selected[*].uprn'
+    },
+    expected: {
+      numSelected: 3,
+      selected: [1, 3]
+    }
+  },
+  {
+    input: {
+      uprn: 123,
+      addressLabel: '123 Red Road'
+    },
+    template: {
+      key: '$.uprn',
+      address: '$.addressLabel'
+    },
+    expected: {
+      key: 123,
+      address: '123 Red Road'
+    }
+  },
+  {
+    input: {
+      uprn: '1234',
+      counter: 9
+    },
+    template: {
+      uprn: { equals: '$.uprn' },
+      counter: { notEquals: '$.counter' }
+    },
+    expected: {
+      uprn: { equals: '1234' },
+      counter: { notEquals: 9 }
+    }
+  },
+  {
+    input: {
       case: {
         id: 'ID-1234'
       }
